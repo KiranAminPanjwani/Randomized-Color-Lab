19a8b9fe119411508879488ef0ebf1f13ff0aea1
Thomas <thomasmartin98@hotmail.com>
2022-09-30 20:38:42 +0100

    remove code smells

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,88 +1,106 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./Home.css";
 
 const Home = () => {
+  const numberOfColors = 6;
+  const defaultHexCodeArray = new Array(numberOfColors).fill("#7a7877");
+  const [hexcode, setHexcode] = useState(defaultHexCodeArray);
+  const [num, setnum] = useState(0);
+  const [isWin, setIsWin] = useState(null);
+  const [endColor, setendColor] = useState("");
 
-    const [hexcode, setHexcode] = useState([]);
-    const [num, setnum] = useState(0);
-    const [isWin, setIsWin] = useState();
-    const [endColor, setendColor] = useState();
-
-    //useeffect is a mountingstate for a component
-    //it is a function who's function is under our control
-    //it is dependant on array = array dependacy; iff array is null, it will run one time else according to array size
-    useEffect(() => {
-        setIsWin("");
-        setHexcode([]);
-        for (let i = 0; i < 6; i++) {
-            setHexcode(hexcode => [...hexcode, "#7a7877"]); //assign grey to all
-        }
-    }, [])
-
-    const randomized_ColorCode = () => {
-        setIsWin("");
-        setHexcode([]);
-        for (var i = 0; i < 6; i++) {
-            setHexcode(hexcode => [...hexcode, `#${Math.floor(Math.random() * 16777000).toString(16)}`])
-            //here hexcode is acting as previous state for new array. everytime new array is generated and replaces the old one.
-            //new random value s generated and array is appended.
-        }
-
-        setnum(Math.floor(Math.random() * 6));
+  const create_new_hex_array = () => {
+    const newArray = new Array(numberOfColors);
+    for (let idx = 0; idx < hexcode.length; idx++) {
+      newArray[idx] = `#${Math.floor(Math.random() * 16777000).toString(16)}`;
     }
+    setHexcode(newArray);
+  };
 
-    const resetColors = () => {
-        setIsWin("");
-        //emptying hexcode
-        setHexcode([]);
-        for (var i = 0; i < 6; i++) {
-            setHexcode(hexcode => [...hexcode, "#7a7877"]);
-        }
+  const resetRandomised = () => {
+    create_new_hex_array();
+    setnum(Math.floor(Math.random() * 6));
+    setIsWin(null);
+  };
+
+  const resetGame = () => {
+    setIsWin(null);
+    setHexcode(defaultHexCodeArray);
+    setendColor("");
+  };
+
+  const checkColor = (hex) => {
+    if (hex === hexcode[num]) {
+      const filledArray = new Array(hexcode.length).fill(hex);
+
+      setendColor(hex);
+      setIsWin(true);
+      setHexcode(filledArray);
+    } else {
+      setIsWin(false);
     }
+  };
 
-    const checkColor = (hex) => {
-        if (hex === hexcode[num]) {
-            setendColor(hexcode[num]);
+  const copyColorToClipboard = (endColor) => {
+    navigator.clipboard.writeText(endColor);
+  };
 
-            setIsWin(true);
-
-            setHexcode([]);
-            for (var i = 0; i < 6; i++) {
-                setHexcode(hexcode => [...hexcode, hex]);
-            }
-
-        } else {
-            setIsWin(false);
-        }
-    }
-
-    const finalColorDisplay = (endColor) => {
-        navigator.clipboard.writeText(endColor);        
-    }
-
-    return (
-        <div className="home">
-
-            <h1 id="headline"><p className="clickText">{isWin === "" ? "Click the button below & " : (isWin ? "You Win" : "You lose")}</p>Generate New <span id="rgbdisplay">{hexcode[num] !== "#7a7877" ? hexcode[num] : "Hex"}</span> Colors</h1>
-            <div id="stripe">
-                <button id="btn1" onClick={randomized_ColorCode}><img src="https://img.icons8.com/external-bearicons-glyph-bearicons/64/000000/external-click-call-to-action-bearicons-glyph-bearicons.png" alt="clickme" id="clickimg" />Random Color</button>
-                <p id="endbutton" onClick={() => resetColors()}>{isWin === true ? (<button id="endbtn">end game?</button>) : ""}</p>
+  return (
+    <div className="home">
+      <h1 id="headline">
+        <p className="clickText">
+          {isWin == null
+            ? "Click the button below & "
+            : isWin
+            ? "You Win"
+            : "You lose"}
+        </p>
+        <p>{`Generate NEW ${
+          hexcode[num] !== "#7a7877" ? hexcode[num] : "Hex"
+        } Colors`}</p>
+      </h1>
+      <div id="stripe">
+        <button id="btn1" onClick={() => resetRandomised()}>
+          <img
+            src="https://img.icons8.com/external-bearicons-glyph-bearicons/64/000000/external-click-call-to-action-bearicons-glyph-bearicons.png"
+            alt="clickme"
+            id="clickimg"
+          />
+          Random Color
+        </button>
+        <p id="endbutton" onClick={() => resetGame()}>
+          {isWin && <button id="endbtn">end game?</button>}
+        </p>
+      </div>
+      <div id="container">
+        <div className="row">
+          {/* map is an array function which maps each value */}
+          {hexcode.map((hex, i) => (
+            <div className="row-child" key={`${hex}-${i}`}>
+              <div
+                className="square"
+                style={{ backgroundColor: `${hex}` }}
+                onClick={() => checkColor(hex)}
+              >
+                <span>
+                  {isWin && (
+                    <button
+                      onClick={() => copyColorToClipboard(endColor)}
+                      style={{
+                        backgroundColor: isWin === true ? `${endColor}` : "",
+                      }}
+                    >
+                      Copy Color
+                    </button>
+                  )}
+                </span>
+              </div>
             </div>
-            <div id="container">
-                <div className="row">
-                    {/* map is an array function which maps each value */}
-                    {hexcode.map((hex, i) => (
-                        <div className="row-child">
-                            <div className="square" key={i} style={{ backgroundColor: `${hex}` }} onClick={() => checkColor(hex)}>
-                                {/* <p><span id="color-display">{hex === "#7a7877" ? " " : hex}</span></p> */}
-                                <span>{isWin === true ? (<button onClick={() => finalColorDisplay(endColor)} style={{backgroundColor:isWin===true? `${endColor}`:""}}>Copy Color</button>) : " "}</span>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
